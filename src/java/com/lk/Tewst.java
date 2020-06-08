19f45fae9ff96b1e57d090e35ea6d70a5fbf1380
KeeveLee <975425885@qq.com>
2020-06-08 09:44:12 +0800

    init

---
+++ src/java/com/lk/Tewst.java
@@ -0,0 +1,13 @@
+package com.lk;
+
+/**
+ * Description: TODO
+ *
+ * @author likai
+ * @date 2020-06-08 09:43
+ */
+public class Tewst {
+    public static void main(String[] args){
+        System.out.println("fasdfasfd");
+    }
+}
